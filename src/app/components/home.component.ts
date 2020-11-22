170cf0500554f0435ac3a76706547ee00e21f008
maha <maha.suguru@gmail.com>
2020-11-22 20:57:33 +0530

    added ng bootstrap and bootstrap

--- src/app/components/home.component.ts
+++ src/app/components/home.component.ts
@@ -4,6 +4,7 @@ import { Component } from "@angular/core";
   selector: "app-home",
   template: `
   <h2>Hello From Home</h2>
+  <h2 class="text-primary">Hello From Home</h2>
   <img src="assets/images/map.png" width="50%" />
 `,
 })
