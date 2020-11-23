3a1d0acff4724a993f9e49e4ea795476b46b99aa
maha <maha.suguru@gmail.com>
2020-11-23 17:19:54 +0530

    ngFor

--- src/app/components/productlist/productlist.component.ts
+++ src/app/components/productlist/productlist.component.ts
@@ -11,14 +11,41 @@ export class ProductlistComponent {
   constructor() { }
 
   pageTitle: string = 'Bike List';
-  product: IProduct = {
-    productName: 'Hero Honda CD 100',
-    description: `India's Most loved bike`,
-    price: 200,
-    releaseDate: '10-10-1990',
-    isActive: false,
-    imageUrl: 'https://via.placeholder.com/200?text=CD100SS',
-  };
+  products: IProduct[] = [
+    {
+      productName: 'Hero Honda CD 100',
+      description: "Most popular Bike of India",
+      releaseDate: "10-08-1990",
+      price: 100,
+      isActive: false,
+      imageUrl: "https://via.placeholder.com/150?text=CD100SS",
+    },
+    {
+      productName: "Honda Hornet",
+      description: "A sports Bike",
+      releaseDate: "10-08-2010",
+      price: 200,
+      isActive: true,
+      imageUrl: "https://via.placeholder.com/150?text=Hornet",
+    },
+    {
+      productName: "Super splendor",
+      description: "A Bike that the nation trusts",
+      releaseDate: "10-08-1980",
+      price: 75,
+      isActive: true,
+
+      imageUrl: "https://via.placeholder.com/150?text=Splendor",
+    },
+    {
+      productName: "Yamaha RX 100",
+      description: "Nostalgic !",
+      releaseDate: "10-08-1987",
+      price: 122,
+      isActive: false,
+      imageUrl: "https://via.placeholder.com/150?text=RX100",
+    },
+  ];
 
   getTitle(): string {
     return 'Hello from Method';
--- src/app/components/products.component.ts
+++ src/app/components/products.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: "app-product",
-  template: "<h2>Hello Inside Product Componnet</h2>",
+  selector: 'app-product',
+  template: '<h2>Hello Inside Product Componnet</h2>',
 })
 export class ProductsComponent {
   constructor() {}
