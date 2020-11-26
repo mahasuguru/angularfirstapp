685c2c76f20b5b56a7b4a0452458b16f3a6c1f0f
maha <maha.suguru@gmail.com>
2020-11-26 16:04:09 +0530

    implemented back button in productdetails

--- src/app/components/productdetails/productdetails.component.ts
+++ src/app/components/productdetails/productdetails.component.ts
@@ -1,5 +1,5 @@
  import { Component, OnInit } from '@angular/core';
- import { ActivatedRoute } from "@angular/router";
+ import { ActivatedRoute, Router  } from "@angular/router";
  import { IProduct } from "src/app/interfaces/product.interface";
  import { ProductService } from "src/app/services/product.service";
 @Component({
@@ -13,12 +13,17 @@ export class ProductdetailsComponent implements OnInit {
   currentProduct: IProduct;
   constructor(
     private activatedRoute: ActivatedRoute,
-    private productService: ProductService
+    private productService: ProductService, private router: Router
   )  { }
 
   ngOnInit(): void {
+    console.log("ActivatedRoute", this.activatedRoute);
+    console.log("Router", this.router);
     this.currentProductId = +this.activatedRoute.snapshot.paramMap.get("id");
     this.loadInitialData();
+  }
+  goBack() {
+    this.router.navigate(["/products"]);
   }
   private loadInitialData() {
     this.productService.getProduct(this.currentProductId).subscribe(
