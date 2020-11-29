e9eb68b0b2fb770d3b3982aeb6e1023ae063332d
maha <maha.suguru@gmail.com>
2020-11-29 09:27:38 +0530

    created login component

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -9,23 +9,17 @@ import {ILogin} from 'src/app/interfaces/login.interface';
 export class LoginComponent implements OnInit {
   details: ILogin;
   originalData: ILogin;
+  data: {
+    emailAddress: string;
+    password: string;
+  } = {
+    emailAddress: "",
+    password: "",
+  };
+
   constructor() { }
-
-
-  ngOnInit() {
-  this.details = {
-    name: '',
-    password: '',
-    subsrcibe: true,
-    gender : '',
-    emailaddress : '',
-        };
-        this.originalData = { ...this.details };
-
-
-  }
-  submitform(form: NgForm){
-    console.log(form.value);
-
+  ngOnInit() {}
+  submitForm(form: NgForm) {
+    console.log("Login form submitted", form);
   }
 }
